f4e69d7497a4b084c06c608821e4c8f3a3e44cb5
professor <professor@L1MP.IPIRANGA.FATEC>
2018-11-22 14:44:55 -0200

    Primeira versão

---
+++ selenium10/src/br/sce/teste/TestaIB.java
@@ -0,0 +1,40 @@
+package br.sce.teste;
+
+import static org.junit.Assert.assertEquals;
+
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestaIB {
+	
+
+	@Test
+	public void test() {
+		System.setProperty("webdriver.chrome.driver", "C:/temp3/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://banco.bradesco/html/classic/index.shtm");
+		// Procura o elemento html de entrada de texto pelo nome
+		WebElement element = driver.findElement(By.id("AGN"));
+		// Digita as informações solicitadas de acordo com o ct
+		element.sendKeys("1111");
+		element = driver.findElement(By.id("CTA"));
+		element.sendKeys("111111");
+		element = driver.findElement(By.id("DIGCTA"));
+		element.sendKeys("1");
+		element = driver.findElement(By.linkText("OK"));
+		element.click();
+		
+		System.out.println("Page title is: " + driver.getTitle());
+		
+		element.submit();
+		System.out.println("Page title is: " + driver.getTitle());
+		assertEquals("Banco Bradesco | Pessoa Física, Exclusive, Prime e Private",driver.getTitle());
+		// Close the browser
+		//driver.quit();
+	}
+
+}
---
+++ selenium10/src/br/sce/teste/TesteWeb.java
@@ -0,0 +1,32 @@
+package br.sce.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteWeb {
+
+	@Test
+	public void test() {
+		System.setProperty("webdriver.chrome.driver", "C:/temp3/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+		// Procura o elemento html de entrada de texto pelo nome
+		WebElement element = driver.findElement(By.name("q"));
+		// Entra o texto de pesquisa
+		element.sendKeys("Selenium webdriver");
+		// Submete o form. WebDriver encontra o form do objeto element
+		element.submit();
+		System.out.println("O titulo da página é: " + driver.getTitle());
+		assertEquals("Selenium webdriver - Pesquisa Google",driver.getTitle());
+		// Fecha o browser
+		driver.quit();
+	}
+
+}
